3f8c13bdf5ab64993b433ad0e9114557b2018523
romel <romelmahmud.dev@gmail.com>
2025-12-04 12:49:59 +0600

    auth middleware implement

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { authServices } from "./auth.service";
+
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await authServices.loginUser(email, password);
+
+    // console.log(result.rows[0]);
+    res.status(201).json({
+      success: true,
+      message: "login successful",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const authController = {
+  loginUser,
+};
